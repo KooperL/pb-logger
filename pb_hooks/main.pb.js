d79f454f86601a02583075b14f3e79952c57b80b
KooperL <kooper.lingohr@gmail.com>
2024-10-29 19:07:00 +1100

    Add QoL hooks, patch styling

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -9,6 +9,130 @@ routerAdd("get", "/realtime", (c) => {
   return c.html(200, html)
 })
 
+// Create a collection for logs if it doesn't already exist
+onAfterBootstrap((e) => {
+  const existingCollection = $app.dao().findCollectionByNameOrId("log")
+  if (existingCollection) {
+    return
+  }
+
+  const collection = new Collection({
+    name: "log",
+    type: "base",
+    system: false,
+    schema: [
+      {
+        name: "title",
+        type: "text",
+        required: false,
+      },
+      {
+        name: "detail",
+        type: "text",
+        required: false,
+      },
+      {
+        name: "channel",
+        type: "text",
+        required: false,
+      },
+      {
+        name: "level",
+        type: "select",
+        required: false,
+        options: {
+          maxSelect: 1,
+          values: [
+            "info",
+            "debug",
+            "log",
+            "warning",
+            "error",
+            "critical"
+          ]
+        }
+      },
+      {
+        name: "submitted_on",
+        type: "date",
+        required: false,
+        options: {
+          min: "",
+          max: ""
+        }
+      },
+      {
+        name: "session",
+        type: "text",
+        required: false,
+      },
+      {
+        name: "session_inc",
+        type: "number",
+        required: false,
+        options: {
+          min: null,
+          max: null,
+          noDecimal: false
+        }
+      },
+      {
+        name: "identifier",
+        type: "text",
+        required: false,
+      },
+      {
+        name: "thread",
+        type: "text",
+        required: false,
+      },
+      {
+        name: "host",
+        type: "text",
+        required: false,
+      },
+      {
+        name: "tenant",
+        type: "text",
+        required: false,
+      },
+      {
+        name: "source",
+        type: "text",
+        required: false,
+      }
+    ],
+    indexes: [],
+    listRule: "",
+    viewRule: "",
+    createRule: "",
+    updateRule: null,
+    deleteRule: null,
+    options: {}
+  });
+
+  $app.dao().saveCollection(collection)
+
+})
+
+// Every night, remove all records that are older than 5 days
+cronAdd("purgeOldRecords", "0 0 * * *", () => {
+  const date = new Date();
+  date.setDate(date.getDate() - 5);
+  const records = $app.dao().findRecordsByFilter(
+    "log",
+    "created < {:date}", "-created",
+    0,
+    0,
+    { date: date.toISOString().replace('T', ' ') },
+  )
+  records.forEach((record) => {
+    $app.dao().deleteRecord(record)
+  })
+})
+
+
+
 routerAdd("post", "api/custom/log", (c) => {
   try {
     const data = $apis.requestInfo(c).data;
